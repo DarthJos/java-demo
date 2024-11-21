823ab9ba15f7c77aee3d3030e848c906d9bbc8a3
Josimar Reyes Campos <darth_jos@hotmail.com>
2024-11-21 13:38:10 -0600

    Repaso | Ejercicio Calcula  | Añade métodos vacios

--- src/main/java/org/jrprojects/calculadora/Calculadora.java
+++ src/main/java/org/jrprojects/calculadora/Calculadora.java
@@ -19,8 +19,27 @@ public class Calculadora {
         System.out.println("Ingresa el 2o número: ");
         String str_num2 = scanner.nextLine();
 
+        //Conversión de números a dobles
+        double num1 = Double.parseDouble(str_num1);
+        double num2 = Integer.parseInt(str_num2);
+        double resultado = 0;
+
         System.out.println(str_num1 + " " + operacion + " " + str_num2);
     }
 
+    private static double suma(double num1, double num2) {
+        return num1 + num2;
+    }
 
+    private static double resta(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    private static double multiplicacion(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    private static double division(double num1, double num2) {
+        return  num1 / num2;
+    }
 }
