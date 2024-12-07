ec90b3b07d300931d7ed3b11095452c0adddf946
Josimar Reyes Campos <darth_jos@hotmail.com>
2024-12-07 02:09:32 -0600

    Repaso | Estudiante | Add class Estudiante

--- src/main/java/org/jrprojects/Main.java
+++ src/main/java/org/jrprojects/Main.java
@@ -1,5 +1,6 @@
 package org.jrprojects;
 
+import org.jrprojects.javacore.Estudiante;
 import org.jrprojects.javacore.VerificadorPalabras;
 
 public class Main {
@@ -13,11 +14,19 @@ public class Main {
         //Calificacion calificacion = new Calificacion();
         //System.out.println("Promedio de las notas: "+calificacion.getPromedio());
 
-        VerificadorPalabras verificador = new VerificadorPalabras();
+        /*VerificadorPalabras verificador = new VerificadorPalabras();
         String frase = "Hola Mundo Java!";
         String palabra = "Java";
         System.out.println("Cantidad de Palabras: " + verificador.getNumPalabras(frase));
         System.out.println("Frase en mayusculas: " + verificador.setToMayus(frase));
-        System.out.println("La palabra '"+palabra+"' se encuentra en la frase? " + verificador.contienePalabra(frase, palabra));
+        System.out.println("La palabra '"+palabra+"' se encuentra en la frase? " + verificador.contienePalabra(frase, palabra));*/
+
+        Estudiante josi = new Estudiante("Josi", 34, 100);
+        Estudiante yvi = new Estudiante("Yvi", 24, 99);
+        Estudiante carito = new Estudiante("Luna Carolina", 1, 99.9);
+
+        System.out.println(josi.isApproved());
+        System.out.println(yvi.getEdad());
+        System.out.println(carito.getNombre());
     }
 }
---
+++ src/main/java/org/jrprojects/javacore/Estudiante.java
@@ -0,0 +1,51 @@
+package org.jrprojects.javacore;
+
+/**
+ * clase Estudiante que contenga:
+ *
+ * Atributos: nombre, edad, calificación.
+ * Métodos:
+ *  Un método para establecer los valores de los atributos.
+ *  Un método para calcular si el estudiante pasa el curso (calificación >= 6).
+ *  Un método para mostrar los datos del estudiante.
+ */
+public class Estudiante {
+
+    private String nombre;
+    private int edad;
+    private double calificacion;
+
+    public Estudiante(String nombre, int edad, double calificacion) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.calificacion = calificacion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public double getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(double calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public String isApproved() {
+        return calificacion >= 6 ? "Pasa el curso" : "No pasa el curso";
+    }
+}
